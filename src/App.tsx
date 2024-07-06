5d8967eac0339293aeb7204aa7107082aefee40e
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2024-07-06 16:41:21 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl text-gray-500 border-b uppercase text-center">
+        Hello world
+      </h1>
+    </div>
+  );
+}
+
+export default App;
